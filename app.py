bb42420efcc1c0d874d4bd680fbd27e8d03c9c4d
ZARIFMAC <uzbzarif@gmail.com>
2023-02-13 10:22:05 +0500

    home page

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ from flask import Flask, request, jsonify
 # Create an instance of Flask
 app = Flask(__name__)
 
+#Create a route for home page
+@app.route('/')
+def home():
+    html = '''
+    <h1> This is a home page </h1>
+    <p> This is a paragraph </p>
+    '''
+    return html
+
 # Create a route
 @app.route('/api', methods=['POST'])
 def api():
