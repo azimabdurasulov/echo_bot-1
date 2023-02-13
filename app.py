1365c45d11a577db4c1896a8906b7bba9bafb85b
ZARIFMAC <uzbzarif@gmail.com>
2023-02-13 10:53:22 +0500

    send message to bot

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-# Import library
+# Import library to create a web app
 from flask import Flask, request, jsonify
+# Import library to get environment variable
+import os 
+# Import library for Telegram bot
+import telegram 
 
 # Create an instance of Flask
 app = Flask(__name__)
+TOKEN = os.environ.get('TOKEN')
+# Create a bot
+bot = telegram.Bot(token=TOKEN)
 
 #Create a route for home page
 @app.route('/')
@@ -17,9 +24,13 @@ def home():
 @app.route('/api', methods=['POST'])
 def api():
     # Get the data from the POST request.
+   
     data = request.get_json(force=True)
-    output = {'message': 'Hello World'}
-    return output
+    chat_id =5575549228
+    # Send a message to the bot
+    bot.send_message(chat_id=chat_id, text='Hello, this is a message from the bot')
+    
+    return jsonify(data)
 
 
 # Run the app
