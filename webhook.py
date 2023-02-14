a1c4ee13cd067bc50c453d82ac8b2c0db91fb32e
azimabdurasulov <azimjon20050604@gmail.com>
2023-02-14 11:54:01 +0500

    webhook.py

---
+++ webhook.py
@@ -0,0 +1,12 @@
+import requests
+import os
+
+url = "https://azim34.pythonanywhere.com/api"
+
+TOKEN = os.environ['TOKEN']
+payload = {
+    "url": url
+}
+
+response = requests.get(f'https://api.telegram.org/bot{TOKEN}/setWebhook', params=payload)
+print(response.status_code)
