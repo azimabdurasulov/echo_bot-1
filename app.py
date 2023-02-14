cbe608f87ec95014816d6c06f3f1f6be294ce028
azimabdurasulov <azimjon20050604@gmail.com>
2023-02-14 20:47:22 +0500

    init

--- app.py
+++ app.py
@@ -1,40 +1,28 @@
-# Import library to create a web app
-from flask import Flask, request, jsonify
-# Import library to get environment variable
-import os 
-# Import library for Telegram bot
-import telegram 
+from flask import Flask, request
+import requests
+import os
+from telegram import Bot, Update
 
-# Create an instance of Flask
+# flask app
 app = Flask(__name__)
-TOKEN = os.environ.get('TOKEN')
-# Create a bot
-bot = telegram.Bot(token=TOKEN)
 
-#Create a route for home page
-@app.route('/')
-def home():
-    html = '''
-    <h1> This is a home page </h1>
-    <p> This is a paragraph </p>
-    '''
-    print(TOKEN)
-    return html
+# bot
+TOKEN = os.environ['TOKEN']
+bot = Bot(TOKEN)
 
-# Create a route
-@app.route('/api')
-def api():
-    # Get the data from the POST request.
-   
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    # get data from request
     data = request.get_json(force=True)
-    chat_id =telegram.Update.message.chat_id
 
-    # Send a message to the bot
-    bot.send_message(chat_id=chat_id, text=telegram.Update.message.text)
-    print(data)
-    return jsonify(data)
+    # update
+    update: Update = Update.de_json(data, bot)
 
+    # get chat_id, text from update
+    chat_id = update.message.chat.id
+    text = update.message.text
 
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
+    # sendMessage
+    bot.send_message(chat_id, text)
+
+    return 'ok'
